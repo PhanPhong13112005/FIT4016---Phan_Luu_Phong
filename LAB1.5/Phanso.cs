37d13eacede342856a911c99f49e6136bd4a491d
Phan Lưu PHong <50897010+PhanPhong13112005@users.noreply.github.com>
2025-04-09 16:48:56 +0700

    Add files via upload

---
+++ LAB1.5/Phanso.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1._5
+{
+    public class PhanSo
+    {
+        public int TuSo { get; set; }
+        public int MauSo { get; set; }
+
+        // Constructor mặc định
+        public PhanSo() { }
+
+        // Constructor khởi tạo với giá trị tử số và mẫu số
+        public PhanSo(int tuSo, int mauSo)
+        {
+            TuSo = tuSo;
+            MauSo = mauSo;
+        }
+
+        // Phương thức nhập phân số từ bàn phím
+        public void NhapPhanSo()
+        {
+            Console.Write("Nhập tử số: ");
+            TuSo = int.Parse(Console.ReadLine());
+            Console.Write("Nhập mẫu số: ");
+            MauSo = int.Parse(Console.ReadLine());
+            // Kiểm tra mẫu số khác 0
+            while (MauSo == 0)
+            {
+                Console.Write("Mẫu số không được bằng 0. Nhập lại mẫu số: ");
+                MauSo = int.Parse(Console.ReadLine());
+            }
+        }
+
+        // Phương thức cộng 2 phân số và trả về kết quả dưới dạng một đối tượng PhanSo mới
+        public static PhanSo CongPhanSo(PhanSo ps1, PhanSo ps2)
+        {
+            // Cộng phân số theo công thức:
+            // a/b + c/d = (a*d + c*b)/(b*d)
+            int tu = ps1.TuSo * ps2.MauSo + ps2.TuSo * ps1.MauSo;
+            int mau = ps1.MauSo * ps2.MauSo;
+            PhanSo ketQua = new PhanSo(tu, mau);
+            ketQua.RutGon();
+            return ketQua;
+        }
+
+        // Phương thức rút gọn phân số bằng cách chia cả tử số và mẫu số cho ước chung lớn nhất (UCLN)
+        public void RutGon()
+        {
+            int ucln = UCLN(Math.Abs(TuSo), Math.Abs(MauSo));
+            TuSo /= ucln;
+            MauSo /= ucln;
+        }
+
+        // Hàm tính ước chung lớn nhất (UCLN) của 2 số
+        private int UCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+        // Ghi đè phương thức ToString để in phân số theo dạng "tử số/mẫu số"
+        public override string ToString()
+        {
+            return $"{TuSo}/{MauSo}";
+        }
+    }
+
+}
---
+++ LAB1.5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using LAB1._5;
+
+namespace SumFractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hỗ trợ in tiếng Việt lên Console
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Tạo danh sách chứa các phân số
+            List<PhanSo> danhSachPhanSo = new List<PhanSo>();
+
+            Console.Write("Nhập số lượng phân số: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Nhập các phân số từ bàn phím
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhập phân số thứ {i + 1}:");
+                PhanSo ps = new PhanSo();
+                ps.NhapPhanSo();
+                danhSachPhanSo.Add(ps);
+            }
+
+            // Tính tổng các phân số trong danh sách
+            // Khởi tạo tổng bằng 0/1
+            PhanSo tong = new PhanSo(0, 1);
+            foreach (var ps in danhSachPhanSo)
+            {
+                tong = PhanSo.CongPhanSo(tong, ps);
+            }
+
+            Console.WriteLine("Tổng các phân số trong danh sách là: " + tong);
+            Console.WriteLine("Nhấn phím bất kỳ để thoát...");
+            Console.ReadLine();
+        }
+    }
+}
