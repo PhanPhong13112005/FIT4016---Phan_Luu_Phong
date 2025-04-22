3c26c1f49f73a0fd72de5cce8fcefc6249d34c65
Phan Lưu PHong <50897010+PhanPhong13112005@users.noreply.github.com>
2025-04-22 23:33:37 +0700

    Add files via upload

---
+++ LAB 2.0/LAB2/Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using LAB2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Todo todo)
+        {
+            // Logic thêm task
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Todo todo)
+        {
+            // Logic sửa task
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
